dd1ba75aed902cc4c13981b421cfff5214babddf
gruberkevin <kevin.gruber@sarix.eu>
2022-11-19 00:24:24 +0100

    Added theme

---
+++ lib/assets/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData themeData = ThemeData(
+  colorScheme: ColorScheme.fromSwatch(
+      brightness: Brightness.dark,
+      primaryColorDark: const Color.fromRGBO(237, 106, 44, 1),
+      accentColor: const Color.fromRGBO(237, 106, 44, 1)),
+);
