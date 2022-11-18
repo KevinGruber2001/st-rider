e39f56b591d4824c322ff7958dbd1c26903edbbf
gruberkevin <kevin.gruber@sarix.eu>
2022-11-19 00:52:26 +0100

    added first page

---
+++ lib/pages/home/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.local_activity),
+          onPressed: () {},
+        ),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.help))],
+        title: const Text("Choose a location"),
+      ),
+    );
+  }
+}
