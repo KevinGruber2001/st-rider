2b39cd55d864f79648c5be09229c790bcfe1fb02
Erik Kiessig <erik.kiessig@tum.de>
2022-11-19 15:10:46 +0100

    REST API Implementation

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
 import 'dart:developer';
+// REST Api
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -137,6 +141,96 @@ class _HomePageState extends State<HomePage> {
                                       origin!.position, destination!.position);
 
                               print(directions);
+
+                              // REST API Get Request to get token
+                              var response = await http.get(Uri.parse(
+                                  'http://131.159.197.39/authenticate'));
+                              var token;
+                              if (response.statusCode == 200) {
+                                // If the server did return a 200 OK response,
+                                // then parse the JSON.
+                                token = jsonDecode(response.body);
+                                print(
+                                    "Statuscode Token Request ${response.statusCode}");
+                                print(token);
+                              } else {
+                                // If the server did not return a 200 OK response,
+                                // then throw an exception.
+                                throw Exception('Failed to load token');
+                              }
+
+                              // REST API PUT Response to send origin and destination
+                              var request = await http.post(
+                                Uri.parse(
+                                    'http://131.159.197.39/rider/registerRequest'),
+                                headers: <String, String>{
+                                  'Content-Type':
+                                      'application/json; charset=UTF-8',
+                                },
+                                body: jsonEncode(<String, dynamic>{
+                                  'token': token,
+                                  'begin': {
+                                    'longitude': origin?.position.longitude,
+                                    'latitude': origin?.position.latitude
+                                  },
+                                  'end': {
+                                    'longitude':
+                                        destination?.position.longitude,
+                                    'latitude': destination?.position.latitude
+                                  }
+                                }),
+                              );
+
+                              print(
+                                  "Statuscode Origin Destination Response ${request.statusCode}");
+
+                              // REST API GET Response Assignment
+                              var responseAssignment = await http.post(
+                                  Uri.parse(
+                                      'http://131.159.197.39/rider/fetchAssignment'),
+                                  headers: <String, String>{
+                                    'Content-Type':
+                                        'application/json; charset=UTF-8'
+                                  },
+                                  body: jsonEncode(
+                                      <String, String>{'token': token}));
+                              var route;
+
+                              if (responseAssignment.statusCode == 200) {
+                                // If the server did return a 200 OK response,
+                                // then parse the JSON.
+                                route = jsonDecode(
+                                    responseAssignment.body)['route'];
+                                print(
+                                    "Statuscode Route Request ${responseAssignment.statusCode}");
+                                print(route);
+
+                                // If there is no route available wait and ask again
+                                while (route == null) {
+                                  Timer(Duration(seconds: 3), () async {
+                                    responseAssignment = await http.post(
+                                        Uri.parse(
+                                            'http://131.159.197.39/rider/fetchAssignment'),
+                                        headers: <String, String>{
+                                          'Content-Type':
+                                              'application/json; charset=UTF-8'
+                                        },
+                                        body: jsonEncode(
+                                            <String, String>{'token': token}));
+                                    if (responseAssignment.statusCode == 200) {
+                                      route = jsonDecode(
+                                          responseAssignment.body)['route'];
+                                      print(
+                                          "Statuscode Route Request ${responseAssignment.statusCode}");
+                                      print(route);
+                                    }
+                                  });
+                                }
+                              } else {
+                                // If the server did not return a 200 OK response,
+                                // then throw an exception.
+                                throw Exception('Failed to load Route');
+                              }
 
                               if (directions != null) {
                                 setState(() {
